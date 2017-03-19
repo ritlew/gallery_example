9373824eb5fc85a2700c3a8b2937788cf5a208cc
samuel <samuel@ritlew.com>
2017-03-19 19:32:28 -0400

    images upload and test

--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def gallery(request):
-    return render(request, 'gallery/gallery.html')
+    images = list()
+    for i in range(6):
+        images.append('lion ({}).jpg'.format(i+1))
+    return render(request, 'gallery/gallery.html', {'filenames': images})
